bd534a4ed0e9244a96508503236e457a5bdf372f
DOLLOR <8869507+DOLLOR@users.noreply.github.com>
2021-02-20 17:27:45 +0800

    Create httpServer.js

---
+++ httpServer.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+const createServer = ({port=8722,host='0.0.0.0',onRequest}) => {
+  return http.createServer((request, response)=>{
+    // post body
+    const chunks = [];
+    request.on('data', chunk => chunks.push(chunk));
+    request.on('end', async () => {
+      const data = Buffer.concat(chunks);
+      const responseText = await onRequest({request,response,data})
+      response
+      .writeHead(200,{
+        'Server': `My server Node.js/${process.version}`,
+        'Access-Control-Allow-Origin': '*',
+        'Cache-Control': 'public, max-age=0',
+      })
+      .end(responseText);
+    });
+  }).listen({
+    port,
+    host,
+  },res=>{
+    console.log(res||'server is running');
+  });
+};
+
+let count = 0;
+createServer({
+  async onRequest({request,response,data}){
+    console.log(
+      `from ${request.connection.remoteAddress}:${request.connection.remotePort}`,
+      request.method,
+      request.url,
+      request.headers,
+      'Data: ',
+      data.toString(),
+      '\n---------------'
+    );
+    return `server ok!\r\n${++count}`;
+  },
+});
